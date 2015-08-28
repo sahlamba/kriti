f11fc1dfa4e7014f5bfad1ecb426eee29b037e73
Sahil Lamba <sahil.lamba95@gmail.com>
2015-08-29 00:05:45 +0530

    Fix user options, modularize Angular code

---
+++ static/js/kriti.profile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Contains all profile page related functions
+
+angular.module('kriti.profile', [])
+  .config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+        .state('profile', {
+          url: '/:id/',
+          templateUrl: '/static/views/profile.html',
+          controller: 'ProfileCtrl',
+          resolve: {
+            items: function (ItemService) {
+              return ItemService.getAllItems();
+            }
+          }
+        });
+    }
+  ]);
+
+angular.module('kriti.profile')
+  .controller('ProfileCtrl', ['$scope', 'items',
+    function ($scope, items) {
+      $scope.items = items;
+    }
+  ]);
