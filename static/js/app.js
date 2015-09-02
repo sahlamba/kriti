9e9ea64d5d77cbeac48efeefefeca16d21e3043e
Sahil Lamba <sahil.lamba95@gmail.com>
2015-09-02 17:48:11 +0530

    Add textAngular as a dependency

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('kriti', [
-    'ngSanitize',
     'ngDialog',
     'ngAnimate',
     'ui.router',
     'angular-loading-bar',
+    'textAngular',
     'kriti.main',
     'kriti.home',
     'kriti.profile'
