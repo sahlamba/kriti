d6cfb986ae61b0af21dceef8304a10b6af18dff8
Sahil Lamba <sahil.lamba95@gmail.com>
2015-10-06 11:46:18 +0530

    Integrate ngDialog for item and search, add services module

--- static/js/kriti.main.js
+++ static/js/kriti.main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Contains the services, directives, filters, main controller
+// Contains the main controller functions
 
 angular.module('kriti.main', [])
   .config(['$urlRouterProvider',
@@ -10,43 +10,39 @@ angular.module('kriti.main', [])
   ]);
 
 angular.module('kriti.main')
-  .factory('ItemService', ['$q', '$http',
-    function ($q, $http) {
-      return {
-        getAllItems: function () {
-          var defer = $q.defer();
-
-          // Make http request here
-          $http.get(api.all_items)
-            .success(function (data) {
-              defer.resolve(data);
-            })
-            .error(function () {
-              console.log("Error in getting data.");
-            });
-
-          return defer.promise;
-        }
-      };
-    }
-  ]);
-
-angular.module('kriti.main')
-  .controller('MainCtrl', ['$rootScope', '$scope', '$state',
-    function ($rootScope, $scope, $state) {
+  .controller('MainCtrl', ['$rootScope', '$scope', '$state', 'SearchService', 'ngDialog',
+    function ($rootScope, $scope, $state, SearchService, ngDialog) {
       $rootScope.appTitle = 'Kriti';
 
       // The 'current_user' object contains info about the logged in user
       $rootScope.current_user = {
-        'name': 'Mila Kunis',
-        'photo': 'http://media.onsugar.com/files/2011/01/05/1/192/1922153/ea8f1ca1b21ed7a8_mila.jpg',
-        'enrolment': '13117060'
+        name: 'Mila Kunis',
+        photo: 'http://media.onsugar.com/files/2011/01/05/1/192/1922153/ea8f1ca1b21ed7a8_mila.jpg',
+        enrolment: '13117060'
       };
 
       $scope.search = {
-        'query': null,
-        'in': 'all',
-        'options': ['all', 'article', 'poem', 'user']
-      }
+        query: null,
+        in: 'all',
+        options: ['all', 'article', 'poem', 'user'],
+        search: function () {
+          if (this.query === null || this.query === undefined || this.query === '') {
+            console.log('No search for you.');
+            return;
+          } else {
+            console.log('Searching...');
+            SearchService.search(this.query).then(function (results) {
+              var options = {
+                template: '/static/views/parts/search.html',
+                className: 'ngdialog-theme-default',
+                overlay: false,
+                scope: $scope,
+                data: results
+              };
+              ngDialog.open(options);
+            });
+          }
+        }
+      };
     }
   ]);
